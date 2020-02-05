3facb323caecce53be092e3865bf110eafa61b09
Thandile Xiphu <thandilexiphu@Thandiles-MacBook-Pro.local>
2020-02-05 07:22:49 +0200

    adds model

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class PhonebookEntry(models.Model):
+    name = models.CharField(max_length=245)
+    surname = models.CharField(max_length=245)
+    number = models.CharField(max_length=245)
+    address_line_1 = models.CharField(max_length=245)
+    address_line_2 = models.CharField(max_length=245)
+    address_line_3 = models.CharField(max_length=245)
+
